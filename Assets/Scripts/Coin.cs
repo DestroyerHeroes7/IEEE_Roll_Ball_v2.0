e70c915af7a2018c58e3d26aa854425494e2e1c0
Emir Elveren <emirelveren7@gmail.com>
2021-01-05 11:13:40 +0300

    Init

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void Start()
+    {
+       
+    }
+    void Update()
+    {
+        transform.Rotate(new Vector3(30, 45, 30) * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+public class Player : MonoBehaviour
+{
+    public float x;
+    public float z;
+    public float speed;
+    public Rigidbody rigidbody;
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector3(x * speed, 0, z * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
